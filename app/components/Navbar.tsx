0abfc9a6ab8f7f501879aa831d29edd305ab264d
Shreejai Raj <shreejairaj@gmail.com>
2024-06-19 20:49:48 +1000

    dark mode switcher shadcn

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import { ModeToggle } from './ModeToggle'
 
 const Navbar = () => {
   return (
     <div className='w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5'>
       <Link href="/" className='font-bold text-3xl'>
-      SJ<span className='text-blue-400'>BLOG</span>
+      SJ<span className='text-primary'>BLOG</span>
       </Link>
-      <div>menu</div>
+      <ModeToggle/>
     </div>
   )
 }
